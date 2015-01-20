eb53b34d299210556482c4e57b2e0e97df529a1e
GoldSciences <pabloaz@gmail.com>
2015-01-20 04:46:01 -0300

    Fixed some naming convention issue
    _ (single underscore) prefix was being used when it's actually
    forbidden, changed to __ (double underscore)

--- GSystem/GCore_macros.h
+++ GSystem/GCore_macros.h
@@ -19,43 +19,43 @@
 #define GODS(TOKEN)		G##TOKEN##_DATA
 
 // This macro is used to declare factory/refcount functions for the structures that implement them
-#define __GDECLARE_CORE_ALLOC_FUNCTIONS(_structName, Token)													\
-	void			gcreateData( _structName** inout_p##Token );											\
-	void			gfreeData( _structName** inout_p##Token );												\
-	void			gfreeData( _structName** inout_p##Token, uint32_t* n##Token##Count );					\
-	_structName*	gacquireData( _structName* in_##Token );												\
-	void			gcopyData( _structName* dst_##Token, const _structName* src_##Token );					\
-	void			gcloneData( _structName** out_p##Token, const _structName* src_##Token );				\
-	int32_t			gcompareData( const _structName* in_##Token##A, const _structName* in_##Token##B );		\
-	bool			gwritableData( const _structName* in_##Token );											\
+#define __GDECLARE_CORE_ALLOC_FUNCTIONS( __structName, Token)												\
+	void			gcreateData( __structName** inout_p##Token );											\
+	void			gfreeData( __structName** inout_p##Token );												\
+	void			gfreeData( __structName** inout_p##Token, uint32_t* n##Token##Count );					\
+	__structName*	gacquireData( __structName* in_##Token );												\
+	void			gcopyData( __structName* dst_##Token, const __structName* src_##Token );				\
+	void			gcloneData( __structName** out_p##Token, const __structName* src_##Token );				\
+	int32_t			gcompareData( const __structName* in_##Token##A, const __structName* in_##Token##B );	\
+	bool			gwritableData( const __structName* in_##Token );										\
 
 // This macro is used to declare read/write functions of the structure data (serialization)
-#define __GDECLARE_CORE_SERIALIZATION_FUNCTIONS(_structName, Token)																	\
-	uint32_t fileDeserializeData( _structName** out_lstDataInstances, uint32_t in_nInstanceCount, FILE* in_fp );					\
-	uint32_t fileSerializeData( _structName* const* in_lstDataInstances, uint32_t in_nInstanceCount, FILE* out_fp );				\
-	uint32_t memDeserializeData( _structName** out_lstDataInstances, uint32_t in_nInstanceCount, const void* in_pMemoryBuffer );	\
-	uint32_t memSerializeData( _structName* const* in_lstDataInstances, uint32_t in_nInstanceCount, void* out_pMemoryBuffer );		\
+#define __GDECLARE_CORE_SERIALIZATION_FUNCTIONS(__structName, Token)																\
+	uint32_t fileDeserializeData( __structName** out_lstDataInstances, uint32_t in_nInstanceCount, FILE* in_fp );					\
+	uint32_t fileSerializeData( __structName* const* in_lstDataInstances, uint32_t in_nInstanceCount, FILE* out_fp );				\
+	uint32_t memDeserializeData( __structName** out_lstDataInstances, uint32_t in_nInstanceCount, const void* in_pMemoryBuffer );	\
+	uint32_t memSerializeData( __structName* const* in_lstDataInstances, uint32_t in_nInstanceCount, void* out_pMemoryBuffer );		\
 
-#define __GDECLARE_CORE_STREAMING_FUNCTIONS( _structName, Token)																			\
-	uint32_t fileReadData( _structName** out_lstDataInstances, uint32_t in_nInstanceCount, FILE* in_fp );									\
-	uint32_t fileWriteData( _structName* const* in_lstDataInstances, uint32_t in_nInstanceCount, FILE* out_fp, _structName* DefaultData );	\
-	uint32_t memReadData( _structName** out_lstDataInstances, uint32_t in_nInstanceCount, const void* in_pMemoryBuffer );					\
-	uint32_t memWriteData( _structName*  const* in_lstDataInstances, uint32_t in_nInstanceCount, void* out_pMemoryBuffer, _structName* DefaultData );
+#define __GDECLARE_CORE_STREAMING_FUNCTIONS( __structName, Token)																				\
+	uint32_t fileReadData( __structName** out_lstDataInstances, uint32_t in_nInstanceCount, FILE* in_fp );										\
+	uint32_t fileWriteData( __structName* const* in_lstDataInstances, uint32_t in_nInstanceCount, FILE* out_fp, __structName* DefaultData );	\
+	uint32_t memReadData( __structName** out_lstDataInstances, uint32_t in_nInstanceCount, const void* in_pMemoryBuffer );						\
+	uint32_t memWriteData( __structName*  const* in_lstDataInstances, uint32_t in_nInstanceCount, void* out_pMemoryBuffer, __structName* DefaultData );
 
 // This macro declares functions to retrieve formatted informative text about the structures. These must be coded individually for each structure 
-#define __GDECLARE_CORE_DEBUG_STRING_FUNCTIONS(_structName, Token)											\
-	void printInfoString( const _structName* in_##Token );													\
-	void getInfoString( wchar_t* dst_pOutputBuffer, uint32_t nBufferSize, const _structName* in_##Token );	\
-	void getInfoString( char* dst_pOutputBuffer, uint32_t nBufferSize, const _structName* in_##Token );
+#define __GDECLARE_CORE_DEBUG_STRING_FUNCTIONS(__structName, Token)											\
+	void printInfoString( const __structName* in_##Token );													\
+	void getInfoString( wchar_t* dst_pOutputBuffer, uint32_t nBufferSize, const __structName* in_##Token );	\
+	void getInfoString( char* dst_pOutputBuffer, uint32_t nBufferSize, const __structName* in_##Token );
 
 // This macro is used to declare all core functions at once
-#define __GDECLARE_CORE_STRUCT_FUNCTIONS( TOKEN, Token )			\
+#define __GDECLARE_CORE_STRUCT_FUNCTIONS( TOKEN, Token )				\
 	__GDECLARE_CORE_ALLOC_FUNCTIONS( __GODS(TOKEN), Token );			\
 	__GDECLARE_CORE_STREAMING_FUNCTIONS( __GODS(TOKEN), Token );		\
 	__GDECLARE_CORE_SERIALIZATION_FUNCTIONS( __GODS(TOKEN), Token );	\
 	__GDECLARE_CORE_DEBUG_STRING_FUNCTIONS( __GODS(TOKEN), Token );
 
-#define __GDECLARE_CORE_STRUCT_TYPEDEFS( TOKEN, Token )	\
+#define __GDECLARE_CORE_STRUCT_TYPEDEFS( TOKEN, Token )			\
 	typedef gbase_ptr_safe<GODS(TOKEN)>			gptr_##Token;	\
 	typedef gcore_ptr<GODS(TOKEN)>				goptr_##Token;	\
 	typedef genius_ptr<GODS(TOKEN)>				ggns_##Token;	\
@@ -65,10 +65,10 @@
 	//void onCreate##Token##Default( ggns_##Token* stuff );
 
 // This macro is used to mark the beginning of a core structure declaration. It must be called to open the declaration of the structure before defining the member list.
-#define GBEGIN_CORE_STRUCT(TOKEN)	\
-	typedef struct __GODS(TOKEN)	 *GODS(TOKEN);			\
-	typedef struct __GODS(TOKEN)		\
-	{								\
+#define GBEGIN_CORE_STRUCT(TOKEN)					\
+	typedef struct __GODS(TOKEN)	 *GODS(TOKEN);	\
+	typedef struct __GODS(TOKEN)					\
+	{												\
 		refcount_t				__nRefCount;
 
 // This macro is used to close the declaration of a core function. It must be called to close the member list.
@@ -108,12 +108,12 @@
 #define GOSH(TOKEN)		G##TOKEN##_HANDLER
 
 // This macro is used to declare factory/refcount functions for the structures that implement them
-#define __GDECLARE_CORE_INTERFACE_FUNCTIONS(_structName, Token)						\
-	void gcreateInterface( _structName** inout_p##Token );							\
-	void gfreeInterface( _structName** inout_p##Token );							\
-	void gfreeInterface( _structName** inout_p##Token, uint32_t* n##Token##Count );	\
-	_structName* gacquireInterface( _structName* in_##Token );						\
-	void gfreeInterfaceArray( _structName** in_lst##Token, uint32_t* nElementCount );
+#define __GDECLARE_CORE_INTERFACE_FUNCTIONS(__structName, Token)						\
+	void gcreateInterface( __structName** inout_p##Token );								\
+	void gfreeInterface( __structName** inout_p##Token );								\
+	void gfreeInterface( __structName** inout_p##Token, uint32_t* n##Token##Count );	\
+	_structName* gacquireInterface( __structName* in_##Token );							\
+	void gfreeInterfaceArray( __structName** in_lst##Token, uint32_t* nElementCount );
 //
 #define __GDECLARE_ALL_INTERFACE_FUNCTIONS(TOKEN, Token)		\
 	__GDECLARE_CORE_INTERFACE_FUNCTIONS(__GOSH(TOKEN), Token)
@@ -124,15 +124,15 @@
 	typedef ginterface_ptr<GOSH(TOKEN)> ghdlr_##Token;
 
 // This macro is used to mark the beginning of a core structure declaration. It must be called to open the declaration of the structure before defining the member list.
-#define GBEGIN_CORE_HANDLER(TOKEN)	\
-	typedef struct __GOSH(TOKEN)	 *GOSH(TOKEN);			\
-	typedef struct __GOSH(TOKEN)		\
-	{								\
+#define GBEGIN_CORE_HANDLER(TOKEN)					\
+	typedef struct __GOSH(TOKEN)	 *GOSH(TOKEN);	\
+	typedef struct __GOSH(TOKEN)					\
+	{												\
 		refcount_t				__nRefCount;
-#define GBEGIN_DERIVED_CORE_HANDLER(TOKEN, ...)	\
-	typedef struct __GOSH(TOKEN)	 *GOSH(TOKEN);			\
-	typedef struct __GOSH(TOKEN) : __VA_ARGS__	\
-	{											\
+#define GBEGIN_DERIVED_CORE_HANDLER(TOKEN, ...)		\
+	typedef struct __GOSH(TOKEN)	 *GOSH(TOKEN);	\
+	typedef struct __GOSH(TOKEN) : __VA_ARGS__		\
+	{												\
 		refcount_t				__nRefCount;
 
 // This macro is used to close the declaration of a core function. It must be called to close the member list.
